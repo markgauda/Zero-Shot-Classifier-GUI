4375e70f554dc9572bbc0756fc8a627c76b6d20d
markgauda <markgaud@buffalo.edu>
2023-04-08 00:51:27 -0400

    added comments

--- classifier_class.py
+++ classifier_class.py
@@ -7,9 +7,23 @@ from pylab import rcParams
 matplotlib.use("TkAgg")
 class Classifier:
     def __init__(self):
+        """Downloads, initilizes and loads the zero shot classifier into memory
+        """
         self.zero_shot_classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 
     def classify_text(self, text_to_classify: str, lables:list, multi_label_arg = True):
+        """Will take in some text, labels, and will produce a zero-shot classification.
+        Returns a figure with the probabilities of each possibility. If multi_label_arg
+        if False, then the probabilities will be normalized to add up to one
+
+        Args:
+            text_to_classify (str): The text you want to classify
+            lables (list): A list of strings for all the classes you want to fit into
+            multi_label_arg (bool, optional): Weather you allow for more than one class or not. Defaults to True.
+
+        Returns:
+            matplotlib.figure: A figure representing the probabilites of the text fitting into each class
+        """
         result = self.zero_shot_classifier(
             sequences = text_to_classify,
             candidate_labels = lables,
@@ -19,7 +33,14 @@ class Classifier:
 
 
     def make_classification_graph(self, results):
+        """Creates the figure from the classification results
 
+        Args:
+            results (_type_): The results from a HuggingFace model pipeline
+
+        Returns:
+            matplotlib.figure: A figure representing the probabilites of the text fitting into each class
+        """
         rcParams['figure.figsize'] = (10, 3)
         plt.clf()
         plt.bar(results["labels"], results["scores"])
